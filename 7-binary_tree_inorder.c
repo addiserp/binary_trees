e96c3e417e2c961062fe595923a357e3825f098f
Mikias Gedlu <addiserp@gmail.com>
2023-10-30 18:30:27 +0300

    0x1D. C - Binary trees 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Print in in-order format.
+ *
+ * @tree: given node
+ * @func: function for printing.
+ *
+ * Return: None.
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL)
+{
+return;
+}
+binary_tree_inorder(tree->left, func);
+func(tree->n);
+binary_tree_inorder(tree->right, func);
+
+}
+
+/**
+ * func - print value.
+ *
+ * @n: given value
+ *
+ * Return: None.
+ */
+
+void func(int n)
+{
+printf("%d\n", n);
+}
