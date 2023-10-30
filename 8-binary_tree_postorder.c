8a27882ed62f4af2337cb8b0046679afdddf779d
Mikias Gedlu <addiserp@gmail.com>
2023-10-30 18:33:33 +0300

    0x1D. C - Binary trees 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Print in post-order format.
+ *
+ * @tree: given node
+ * @func: function for printing.
+ *
+ * Return: None.
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL)
+{
+return;
+}
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
+}
+
+/**
+ * func - print value.
+ *
+ * @n: given value
+ *
+ * Return: None.
+ */
+
+void func(int n)
+{
+printf("%d\n", n);
+}
