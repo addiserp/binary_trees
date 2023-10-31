554185c365415fb780fad4811776130decad17eb
Mikias Gedlu <addiserp@gmail.com>
2023-10-31 14:37:48 +0300

    0x1D. C - Binary trees hot fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,12 +11,12 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+binary_tree_t *newNode;
 if (parent == NULL)
 {
 return (NULL);
 }
 
-binary_tree_t *newNode;
 newNode = binary_tree_node(parent, value);
 if (newNode == NULL)
 {
