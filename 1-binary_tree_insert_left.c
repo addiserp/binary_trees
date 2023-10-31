2913d49b1aec1813db7e0f4befd3c9d88e522a84
Mikias Gedlu <addiserp@gmail.com>
2023-10-31 13:57:22 +0300

    0x1D. C - Binary trees hot fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,32 +1,31 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_left - Creates a binary tree node.
- *
- * @parent: partent of the node
- * @value: value of the node
- *
- * Return: a new node.
+ * binary_tree_insert_left - Add a node in the left of the parent
+ * If it exists it move down one level and add the new node first
+ * @parent: The parent of the specified node
+ * @value: The value of the node
+ * Return: NULL if it fails or the new node
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-if (parent == NULL)
-{
-return (NULL);
-}
+	binary_tree_t *new_node;
 
-struct binary_tree_s *newNode;
-newNode = binary_tree_node(parent, value);
-if (newNode == NULL)
-{
-return (NULL);
-}
-if (parent->left != NULL)
-{
-newNode->left = parent->left;
-parent->left->parent = newNode;
-}
-parent->left = newNode;
-	return (newNode);
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
 }
