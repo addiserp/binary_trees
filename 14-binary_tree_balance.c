2e649f6cf4fa93ddf45697e1932ecdb2dbce16b4
Mikias Gedlu <addiserp@gmail.com>
2023-10-31 15:34:38 +0300

    0x1D. C - Binary trees hot fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,33 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - a function that measures
- * the balance factor of a binary tree
- *
- * @tree: given node
- *
- * Return: balance.
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-if (tree == NULL)
-{
-return (0);
-}
-size_t balance = 0;
-size_t countl = 0;
-size_t countr = 0;
-
-if (tree)
-{
-countl = ((int)binary_tree_height_2(tree->left));
-countr = ((int)binary_tree_height_2(tree->right));
-balance = countr - countl;
-}
-return (balance);
-}
-
-/**
  * binary_tree_height - a function that measures
  * the height of a binary tree.
  *
@@ -61,4 +34,30 @@ else
 return (countr);
 }
 }
+}
+/**
+ * binary_tree_balance - a function that measures
+ * the balance factor of a binary tree
+ *
+ * @tree: given node
+ *
+ * Return: balance.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+if (tree == NULL)
+{
+return (0);
+}
+size_t balance = 0;
+size_t countl = 0;
+size_t countr = 0;
+
+if (tree)
+{
+countl = ((int)binary_tree_height_2(tree->left));
+countr = ((int)binary_tree_height_2(tree->right));
+balance = countr - countl;
+}
+return (balance);
 }
