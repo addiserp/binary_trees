7ad94a06404cdc9055de8eda936c82277c3b68e5
Mikias Gedlu <addiserp@gmail.com>
2023-10-31 16:17:48 +0300

    0x1D. C - Binary trees 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -5,7 +5,7 @@
  * the sibling of a node
  * if a binary tree is perfect
  *
- * @tree: given node
+ * @node: given node
  *
  * Return: None.
  */
@@ -13,10 +13,10 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 if (node == NULL || node->parent == NULL)
-	{
-		return (NULL);
-	}
-	if (node->parent->left == node)
-		return (node->parent->right);
-	return (node->parent->left);
+{
+return (NULL);
+}
+if (node->parent->left == node)
+return (node->parent->right);
+return (node->parent->left);
 }
